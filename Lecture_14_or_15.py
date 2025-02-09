6e98c5dead71fbca0136d6de06e5e07dca2cf1f7
SUHANI <Pushpendrarawat868@gmail.com>
2025-02-09 22:19:26 +0530

    Add files via upload

---
+++ Lecture_14_or_15.py
@@ -0,0 +1,20 @@
+"""Central Tradencies:
+    mean , median and Mode 
+median is more statistics"""
+
+"""negativity should not impact positivity that why we square in variance method
+line of best fit = minimum RMSE"""
+
+
+t"""tail heavy histogram:
+    bell shaped curve"""
+    
+"""Training and test set:
+    training set: generally conatin 80% of the data
+    test set: generally contain 20% of the data
+    test set should be randomly selected
+    random sampling is done by using np.random.permutation() method
+    np.random.seed(42) is used to generate the same random number for every run
+    np.random.seed() is used to generate different random number for every run"""
+    
+    
